1365b7ddeb259da2aa57ebc956e21b5c3c254e9c
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-09-23 22:14:38 +0300

    modified SQLQuery3.sql

--- SQL/SQLQuery3.sql
+++ SQL/SQLQuery3.sql
@@ -13,6 +13,7 @@ INSERT INTO Students values('Rajini',30,'Matala');
 INSERT INTO Students values('Raam',22,'Kandy');
 INSERT INTO Students values('Madhan',20,'Colombo');
 INSERT INTO Students values('Ajith',15,'Anurathapura');
+INSERT INTO Students values('Ak',24,'Kurunagala');
 
 SELECT * FROM Students;
 
@@ -35,3 +36,14 @@ SELECT * FROM Students ORDER BY name ASC;
 
 SELECT name FROM Students WHERE age>=20;
 
+SELECT * FROM Students WHERE NOT address='jaffna';
+
+UPDATE Students SET age=28 WHERE name='Ajith';
+
+UPDATE Students SET address='Puttalam' WHERE name='Rajini';
+
+UPDATE Students SET address='Vavuniya' WHERE age=28;
+
+DELETE FROM Students WHERE name ='Ak';
+
+SELECT * FROM Students;
