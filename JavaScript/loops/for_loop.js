9553b907f6471654f2ec885e203e4b0391d2e1f8
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-11-03 15:42:11 +0300

    created JavaScript

---
+++ JavaScript/loops/for_loop.js
@@ -0,0 +1,9 @@
+
+for(let i=0; i<5; i++)
+
+    console.log("Hello World");
+    console.log("Hello");
+
+for(let i=5; i>3; i--)
+    console.log("Akeel");
+
---
+++ JavaScript/loops/while_loop.js
@@ -0,0 +1,16 @@
+let i = 1;
+while(i<5)
+{
+    console.log(i);
+    i++;
+}
+
+let k=1;
+let j=8;
+
+do{
+    console.log(k);
+    k++;
+    j--;
+}
+while(k<10 && j>5);
